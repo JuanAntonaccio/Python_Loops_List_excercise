1c48eb68bb51cd7ecf40a29c27bd413e14bfee05
JuanAntonaccio <jmantonaccio@gmail.com>
2021-10-15 21:44:17 +0000

    comenzando loops y lists

--- .learn/resets/01.1-Access-and-Retrieve/app.py
+++ .learn/resets/01.1-Access-and-Retrieve/app.py
@@ -2,8 +2,7 @@
 my_list = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 
 # 1. print the item here
-print(my_list[2])
+
 # 2. change the position were 'thursday' is to None
-mi_list[3]="None"
+
 # 3. print that position now here
-print(mi_list[3])
