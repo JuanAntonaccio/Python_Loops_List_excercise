43d228f8b96a6e882bf2bba4de06db2eb1db364c
JuanAntonaccio <jmantonaccio@gmail.com>
2021-10-17 14:54:38 +0000

    terminado con 5 errores

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,4 +1,17 @@
-
+def lyrics_generator(list):
+    cadena=""
+    contador=0
+    for i in list:
+        if i==0:
+            cadena+="Boom "
+            contador=0
+        if i==1:
+            cadena+="Drop the base "    
+            contador+=1
+            if contador==3:
+                cadena+="!!!Break the base!!! "
+                contador=0
+    return cadena
 
 # Your code go above, nothing to change after this line:
 print(lyrics_generator([0,0,1,1,0,0,0]))
